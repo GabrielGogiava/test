1c077840b982608f71ae523d032ee6459f969704
GabrielGogiava <g.gogiava@psp.local>
2024-01-04 23:17:01 +0400

    ddddd

---
+++ dist/aaa.js
@@ -0,0 +1,5 @@
+"use strict";
+const fullName = "Gabriel";
+const age = 25;
+console.info(`My name is ${fullName} and my age is ${age}`);
+//# sourceMappingURL=aaa.js.map
---
+++ src/aaa.ts
@@ -0,0 +1,4 @@
+const fullName: string = "Gabriel";
+const age: number = 25;
+
+console.info(`My name is ${fullName} and my age is ${age}`);
